d21a4c726ee42fd8a894ce4d151ac0180ec88f91
cheesegratin4 <kimchimanchan@gmail.com>
2025-12-10 00:26:31 +0900

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,92 @@
+export const siteConfig = {
+  name: "Yim Songchan",
+  title: "Computer Science Student (2024~)",
+  description: "Portfolio website of Ryan Fitzgerald",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "kimchimanchan@gmail.com",
+    github: "https://github.com/cheesegratin4",
+  },
+  aboutMe_0:
+    "세부 인적사항 - 생략",
+  aboutMe_1:
+    "Tel) 010-2558-3304",
+  aboutMe:
+    `한경대학교 - 컴퓨터응용수학부 - 소프트웨어 & 서비스컴퓨팅 (컴퓨터공학)
+    : 재학 (2024~)`,
+  skills: ["Javascript", "Python", "C", "C++", "Svelte", "CSS"],
+  projects: [
+    {
+      name: "Air Pollution Inquiry Mashup",
+      description:
+        `실시간 대기오염정보 조회 서비스 :
+        여러가지 API와 부트스트랩CSS를 이용한 매시업
+        2학년 2학기 웹프로그래밍 학습 예제 수행 결과물 23`,
+      link: "https://2024810083-22web23.netlify.app/",
+      skills: ["Javascript", "CSS"],
+    },
+    {
+      name: "서울시 문화행사 알리미",
+      description:
+        `서울시 문화행사 알리미 PWA :
+        API와 부트스트랩CSS를 이용한 Web App을 PWA로 가공하는
+        2학년 2학기 웹프로그래밍 학습 예제 수행 결과물 24`,
+      link: "https://2024810083-22web24-2.netlify.app/",
+      skills: ["Javascript", "CSS"],
+    },
+    {
+      name: "Kakao Map을 이용한 서울시 공공도서관 검색 Mashup",
+      description:
+        `서울시 공공도서관 검색 App :
+        여러가지 API와 부트스트랩CSS를 이용한 매시업
+        2학년 2학기 웹프로그래밍 학습 예제 수행 결과물 22`,
+      link: "https://2024810083-22web22.netlify.app/",
+      skills: ["Javascript", "CSS"],
+    },
+  ],
+  experience: [
+    /* 형식 참조를 위해 남겨둠
+    {
+      company: "Tech Company",
+      title: "Senior Software Engineer",
+      dateRange: "Jan 2022 - Present",
+      bullets: [
+        "Led development of microservices architecture serving 1M+ users",
+        "Reduced API response times by 40% through optimization",
+        "Mentored team of 5 junior developers",
+      ],
+    },
+    {
+      company: "Startup Inc",
+      title: "Full Stack Developer",
+      dateRange: "Jun 2020 - Dec 2021",
+      bullets: [
+        "Built and launched MVP product from scratch using React and Node.js",
+        "Implemented CI/CD pipeline reducing deployment time by 60%",
+        "Collaborated with product team to define technical requirements",
+      ],
+    },
+    {
+      company: "Digital Agency",
+      title: "Frontend Developer",
+      dateRange: "Aug 2018 - May 2020",
+      bullets: [
+        "Developed responsive web applications for 20+ clients",
+        "Improved site performance scores by 35% on average",
+        "Introduced modern JavaScript frameworks to legacy codebases",
+      ],
+    },
+    */
+  ],
+  education: [
+    {
+      school: "한경대학교",
+      degree: "컴퓨터응용수학부 - 소프트웨어 & 서비스컴퓨팅 (컴퓨터공학)",
+      dateRange: "2024 ~",
+      achievements: [
+        "2025년(현재) 2학년 재학중",
+
+      ],
+    },
+  ],
+};
